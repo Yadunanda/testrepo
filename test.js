4501d62aa7f760bbdb6f933c4c4f103505bcb03d
Yadunanda <yadunandamukundhan@gmail.com>
2021-05-31 01:41:43 -0400

    aaa

--- test.js
+++ test.js
@@ -2,11 +2,6 @@ console.log("file One");
 console.log("file two");
 console.log("file three");
 console.log("Yadunanda's file.js");
-<<<<<<< HEAD
-console.log("this is the task done in task one ")
-console.log("Yadunanda Change 2")
-=======
 console.log("this is the task done in task one ");
 console.log("Change made by the other person");
-
->>>>>>> main
+console.log("Tasks done in task one");
